9d53c3747c75f785c4426497645cea228e48739f
Sigmanificient <edhyjox@gmail.com>
2021-05-20 15:00:51 +0200

    Improving index

--- src/Public/index.php
+++ src/Public/index.php
@@ -2,10 +2,7 @@
 define('ROOT', dirname(__DIR__));
 define('SITE', ($_SERVER['SERVER_PORT'] == 443) ? "https://": "http://" . $_SERVER['SERVER_NAME']);
 
-var_dump(ROOT);
-var_dump(SITE);
+session_start();
 
 require_once ROOT . '/Core/Router.php';
-
-$router = new Router();
-$router->get_route();
+Router::getInstance()->route();
