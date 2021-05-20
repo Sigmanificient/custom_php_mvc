c6e92a84b75bd2df3e8816a2558eb7493b3f82a3
Sigmanificient <edhyjox@gmail.com>
2021-05-20 23:53:57 +0200

    render method

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -4,4 +4,15 @@
 abstract class Controller
 {
     public string $default_action  = '404';
+
+    protected function render(string $file, array $data = [])
+    {
+        $ext = empty($data) ? 'html' : 'php';
+
+        ob_start();
+        require_once ROOT . '/views/' . basename(get_class($this), 'Controller') . '/' . $file . '.' . $ext;
+        $content = ob_get_clean();
+
+        require_once ROOT . '/views/template.phtml';
+    }
 }
