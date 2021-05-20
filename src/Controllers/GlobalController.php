dc992a53654e45b2b453b5b3dbecd3ebd028cb8b
Sigmanificient <edhyjox@gmail.com>
2021-05-21 00:18:14 +0200

    about page

--- src/Controllers/GlobalController.php
+++ src/Controllers/GlobalController.php
@@ -11,8 +11,8 @@ class GlobalController extends Controller
         $this->render('index');
     }
 
-    public function foo()
+    public function about()
     {
-        echo "foo";
+        $this->render('about');
     }
 }
