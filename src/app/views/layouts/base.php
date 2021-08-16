8d9a655090f26c82f8ae7a3d695831577c8ee5cd
Yohann Boniface <edhyjox@gmail.com>
2021-08-16 18:27:29 +0200

    integrating ui from master

--- src/app/views/layouts/base.php
+++ src/app/views/layouts/base.php
@@ -1,3 +1,6 @@
+<?php
+/* @var string $content */
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,9 +8,36 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title>Custom Php MVC</title>
+
+    <link rel="stylesheet" href="/css/main.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Pattaya&family=Poppins&display=swap" rel="stylesheet">
 </head>
-<body>
-    {{content}}
+<body class="grid flat">
+<header class="grid">
+    <div class="container flex">
+        <a href="/">Home</a>
+        <nav class="navbar flex">
+            <a href="/Articles">Articles</a>
+            <a href="/About">About</a>
+            <a href="/User/login">Login</a>
+        </nav>
+    </div>
+</header>
+<main class="container flat grid">
+    <div class="long-container flat grid">
+        {{content}}
+    </div>
+</main>
+<footer class="grid">
+    <div class="container flex flat">
+        <p>Made By Boniface Yohann</p>
+        <a href="https://github.com/Sigmanificient/custom_php_mvc">
+            <img src="/svg/github_icon.svg" height="40px" alt="Github">
+        </a>
+    </div>
+</footer>
 </body>
 </html>
