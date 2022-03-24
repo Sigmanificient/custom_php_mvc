ef1954ce44bd80e4fc09c8abbda7751809fd80ad
sigmanificient <sigmanificient@gmail.com>
2022-03-24 02:15:42 +0100

    :truck: Moving composer to be part of the src

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,11 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
 define('ROOT_DIR', dirname(__DIR__));
-require_once dirname(ROOT_DIR) . '/vendor/autoload.php';
+require_once ROOT_DIR . '/vendor/autoload.php';
 
 use mvc\core\Application;
 
